a98504b8fd98d9776852594fa4c43134aa643f35
Alex Moreno <alejandro.moreno@acquia.com>
2020-06-19 14:34:39 +0100

    Added BackgroundCommands toolkit.

---
+++ src/AcsfLock.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drush\Commands\acsf_custom;
+
+class AcsfLock {
+
+  private $flagsFolder = "/tmp/";
+
+  public function __construct($folder) {
+    $this->flagsFolder = $folder;
+  }
+
+  /**
+   * @throws \Exception
+   */
+  public function getLock($id) {
+    $lockFile = $id . ".lock";
+
+    $fileManager = new AcsfFileManager();
+    $fileManager->createFile($this->flagsFolder . $lockFile, '');
+  }
+
+  /**
+   * @param $id
+   */
+  public function releaseLock($id) {
+      unlink($this->flagsFolder . $id . ".lock");
+  }
+
+  public function doesLockExist($id) {
+    return $this->flagsFolder . $id;
+  }
+}
