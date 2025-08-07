cd81694eb41899df186d45b5397eade97c4927e1
joshirohit100 <joshi.rohit100@gmail.com>
2025-08-07 11:38:34 +0530

    Remove un-used functions.

--- AcsfToolsUtils.php
+++ AcsfToolsUtils.php
@@ -7,7 +7,6 @@
 namespace Drush\Commands\acsf_tools;
 
 use Drush\Drush;
-use Symfony\Component\Yaml\Yaml;
 use Drush\Commands\DrushCommands;
 use Drush\Exceptions\UserAbortException;
 
@@ -56,16 +55,6 @@ class AcsfToolsUtils extends DrushCommands {
   }
 
   /**
-   * Utility function to retrieve a list of sites remotely, via the API.
-   *
-   * @return array|bool
-   */
-  function getRemoteSites($config, $env = 'prod') {
-    $sites_url = $this->getFactoryUrl($config, '/api/v1/sites?limit=100', $env);
-    return $this->curlWrapper($config->username, $config->password, $sites_url)->sites;
-  }
-
-  /**
    * Utility function to prompt the user for confirmation they want to run a
    * command against all sites in their Factory.
    *
@@ -103,92 +92,6 @@ class AcsfToolsUtils extends DrushCommands {
         $this->recursivePrint($value, $indent + 2);
       }
     }
-  }
-
-  /**
-   * Utility function to retrieve locally stored REST API connection info.
-   *
-   * @return mixed
-   */
-  public function getRestConfig($path = NULL) {
-
-    if ($path == NULL) {
-      $path = realpath(dirname(__FILE__));
-    }
-
-    $yaml = Yaml::parse(file_get_contents($path . '/acsf_tools_config.yml'));
-    if ($yaml === FALSE) {
-      $error  = 'acsf_tools_config.yml not found. Make sure to copy/rename ';
-      $error .= 'acsf_tools_config.default.yml and set the appropriate ';
-      $error .= 'connection info.';
-      $this->logger()->error(dt($error));
-    }
-
-    $config = new \stdClass();
-    $config->site_id = $yaml['site_id'];
-    $config->username = $yaml['rest_api_user'];
-    $config->password = $yaml['rest_api_key'];
-    $config->prod_uri = $yaml['rest_factories']['prod'];
-    $config->test_uri = $yaml['rest_factories']['test'];
-    $config->dev_uri = $yaml['rest_factories']['dev'];
-    $config->root_domain = $yaml['root_domain'];
-    $config->subdomain_pattern = $yaml['subdomain_pattern'];
-    $config->prod_web = $yaml['prod_web'];
-    $config->dev_web = $yaml['dev_web'];
-    $config->email_logs_from = $yaml['email_logs_from'];
-    $config->email_logs_to = $yaml['email_logs_to'];
-
-    return $config;
-  }
-
-  /**
-   * Utility function to retrieve the correct factory URI given an environment and desired path.
-   *
-   * @param $config
-   * @param string $path
-   * @param string $env
-   * @return string
-   */
-  public function getFactoryUrl($config, $path = '', $env = 'prod') {
-
-    switch ($env) {
-      case 'dev':
-        $factory_url = $config->dev_uri . $path;
-        break;
-      case 'test':
-        $factory_url = $config->test_uri . $path;
-        break;
-      default:
-        $factory_url = $config->prod_uri . $path;
-        break;
-    }
-
-    return $factory_url;
-  }
-
-  /**
-   * Helper script to abstract curl requests into a single function. Handles both
-   * GET and POST, depending on whether $data is defined or not.
-   *
-   * @param $username
-   * @param $password
-   * @param $url
-   * @param array $data
-   * @return mixed
-   */
-  public function curlWrapper($username, $password, $url, $data = array()) {
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-    if (!empty($data)) {
-      curl_setopt($ch, CURLOPT_POST, count($data));
-      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-    }
-    $result = json_decode(curl_exec($ch));
-    curl_close($ch);
-    return $result;
   }
 
   /**
